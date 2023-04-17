7d02556251e4630cbfc3b0e83500b0628e116ac7
Andrew Le <112992941+ale202008@users.noreply.github.com>
2023-04-16 19:57:22 -0500

    Began working on directory page.

--- TASchedulerWebApp/views.py
+++ TASchedulerWebApp/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import Supervisor, Instructor, TeacherAssistant, Course, LabSection, LectureSection
 
-# Create your views here.
-
-
 class Directory(View):
     def get(self, request):
         return render(request, "directory.html",{})
