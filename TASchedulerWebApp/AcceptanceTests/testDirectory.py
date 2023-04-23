7032a0de37446916dab4c7a24e3de0917df47d5c
Andrew Le <112992941+ale202008@users.noreply.github.com>
2023-04-22 23:54:24 -0500

    Cleaned up the code in testDirectory.py. Split the asserts into different tests and split the methods into classes based on user status.

--- TASchedulerWebApp/AcceptanceTests/testDirectory.py
+++ TASchedulerWebApp/AcceptanceTests/testDirectory.py
@@ -6,61 +6,89 @@ from django.urls import reverse
 class DirectorySuperUserRedirectTestCases(TestCase):
     def setUp(self):
         self.UserClient = Client()
-        self.User = User.objects.create(username = "Taylor", password = "Swift")
+        self.User = User.objects.create(username = "Taylor", password = "Swift", is_superuser=True)
         self.User.save()
         self.directory_url = reverse('directory')
-
-    def test_CorrectSuperUserDisplay(self):
-        # Checks that given the exising user account is of SuperUser status that it properly
-        # displays the form that is built by the function Directory as options is equal to the
-        # buttons displayed. Maybe a unit test rather than an acceptance test.
-        self.User.is_superuser = True
-        self.User.save()
         # Forces the login for the given user, pretty useful.
         self.UserClient.force_login(self.User)
         # Gets the information that is presented by the Directory function given a user
         # is logged in. In this case it is through the Client().
-        resp = self.UserClient.get(self.directory_url)
+        self.resp = self.UserClient.get(self.directory_url)
 
-        self.assertContains(resp, 'Courses', msg_prefix="Course is not displayed")
-        self.assertContains(resp, 'Account Info', msg_prefix="Account Info is not displayed")
-        self.assertContains(resp, 'Notifications', msg_prefix="Notifications is not displayed")
-        self.assertContains(resp, 'Sections', msg_prefix="Sections is not displayed")
-        self.assertContains(resp, 'TAs', msg_prefix="TA is not displayed")
-        self.assertContains(resp, 'Instructors', msg_prefix="Instructors is not displayed")
-        self.assertContains(resp, 'Create Course', msg_prefix="Create Course is not displayed")
-        self.assertContains(resp, 'Create Section', msg_prefix="Create Section is not displayed")
-        self.assertContains(resp, 'Create Account', msg_prefix="Create Account not displayed")
+    def test_SuperUserCourseRedirect(self):
+        self.assertContains(self.resp, 'Courses', msg_prefix="Course is not displayed")
+    def test_SuperUserAccountInfoRedirect(self):
+        self.assertContains(self.resp, 'Account Info', msg_prefix="Account Info is not displayed")
+    def test_SuperUserNotificationsRedirect(self):
+        self.assertContains(self.resp, 'Notifications', msg_prefix="Notifications is not displayed")
+    def test_SuperUserSectionsRedirect(self):
+        self.assertContains(self.resp, 'Sections', msg_prefix="Sections is not displayed")
+    def test_SuperUserTARedirect(self):
+        self.assertContains(self.resp, 'TAs', msg_prefix="TA is not displayed")
+    def test_SuperUserInstructorsRedirect(self):
+        self.assertContains(self.resp, 'Instructors', msg_prefix="Instructors is not displayed")
+    def test_SuperUserCreateCourseRedirect(self):
+        self.assertContains(self.resp, 'Create Course', msg_prefix="Create Course is not displayed")
+    def test_SuperUserCreateSectionRedirect(self):
+        self.assertContains(self.resp, 'Create Section', msg_prefix="Create Section is not displayed")
+    def test_SuperUserCreateAccountRedirect(self):
+        self.assertContains(self.resp, 'Create Account', msg_prefix="Create Account not displayed")
 
-    def test_CorrectInstructorDisplay(self):
-        # Similar to the previous test for SuperUser, except we also check that the form
-        # does not contain any unpermitted buttons.
-        self.User.is_staff = True
+class DirectoryInstructorRedirectsTestCases(TestCase):
+    def setUp(self):
+        self.UserClient = Client()
+        self.User = User.objects.create(username="Taylor", password="Swift", is_staff=True)
         self.User.save()
+        self.directory_url = reverse('directory')
         self.UserClient.force_login(self.User)
-        resp = self.UserClient.get(self.directory_url)
+        self.resp = self.UserClient.get(self.directory_url)
 
-        self.assertContains(resp, 'Courses', msg_prefix="Course is not displayed")
-        self.assertContains(resp, 'Account Info', msg_prefix="Account Info is not displayed")
-        self.assertContains(resp, 'Notifications', msg_prefix="Notifications is not displayed")
-        self.assertContains(resp, 'Sections', msg_prefix="Sections is not displayed")
-        self.assertContains(resp, 'TAs', msg_prefix="TA is not displayed")
-        self.assertNotContains(resp, 'Instructors', msg_prefix="Instructors is displayed")
-        self.assertNotContains(resp, 'Create Course', msg_prefix="Create Course is displayed")
-        self.assertNotContains(resp, 'Create Section', msg_prefix="Create Section is displayed")
-        self.assertNotContains(resp, 'Create Account', msg_prefix="Create Account is displayed")
+    def test_InstructorCourseRedirect(self):
+        self.assertContains(self.resp, 'Courses', msg_prefix="Course is not displayed")
+    def test_InstructorAccountInfoRedirect(self):
+        self.assertContains(self.resp, 'Account Info', msg_prefix="Account Info is not displayed")
+    def test_InstructorNotificationsRedirect(self):
+        self.assertContains(self.resp, 'Notifications', msg_prefix="Notifications is not displayed")
+    def test_InstructorSectionsRedirect(self):
+        self.assertContains(self.resp, 'Sections', msg_prefix="Sections is not displayed")
+    def test_InstructorTARedirect(self):
+        self.assertContains(self.resp, 'TAs', msg_prefix="TA is not displayed")
+    def test_InstructorInstructorsRedirect(self):
+        self.assertNotContains(self.resp, 'Instructors', msg_prefix="Instructors is displayed")
+    def test_InstructorCreateCourseRedirect(self):
+        self.assertNotContains(self.resp, 'Create Course', msg_prefix="Create Course is displayed")
+    def test_InstructorCreateSectionRedirect(self):
+        self.assertNotContains(self.resp, 'Create Section', msg_prefix="Create Section is displayed")
+    def test_InstructorCreateAccountRedirect(self):
+        self.assertNotContains(self.resp, 'Create Account', msg_prefix="Create Account is displayed")
 
-    def test_CorrectTADisplay(self):
-        # Same as the previous code
+class DirectoryTARedirectsTestCases(TestCase):
+    def setUp(self):
+        self.UserClient = Client()
+        self.User = User.objects.create(username="Taylor", password="Swift")
+        self.User.save()
+        self.directory_url = reverse('directory')
         self.UserClient.force_login(self.User)
-        resp = self.UserClient.get(self.directory_url)
+        self.resp = self.UserClient.get(self.directory_url)
 
-        self.assertContains(resp, 'Courses', msg_prefix="Course is not displayed")
-        self.assertContains(resp, 'Account Info', msg_prefix="Account Info is not displayed")
-        self.assertContains(resp, 'Notifications', msg_prefix="Notifications is not displayed")
-        self.assertContains(resp, 'Sections', msg_prefix="Sections is not displayed")
-        self.assertContains(resp, 'TAs', msg_prefix="TA is not displayed")
-        self.assertNotContains(resp, 'Instructors', msg_prefix="Instructors is isplayed")
-        self.assertNotContains(resp, 'Create Course', msg_prefix="Create Course is displayed")
-        self.assertNotContains(resp, 'Create Section', msg_prefix="Create Section is displayed")
-        self.assertNotContains(resp, 'Create Account', msg_prefix="Create Account is displayed")
+    def test_InstructorCourseRedirect(self):
+        self.assertContains(self.resp, 'Courses', msg_prefix="Course is not displayed")
+    def test_InstructorAccountInfoRedirect(self):
+        self.assertContains(self.resp, 'Account Info', msg_prefix="Account Info is not displayed")
+    def test_InstructorNotificationsRedirect(self):
+        self.assertContains(self.resp, 'Notifications', msg_prefix="Notifications is not displayed")
+    def test_InstructorSectionsRedirect(self):
+        self.assertContains(self.resp, 'Sections', msg_prefix="Sections is not displayed")
+    def test_InstructorTARedirect(self):
+        self.assertContains(self.resp, 'TAs', msg_prefix="TA is not displayed")
+    def test_InstructorInstructorsRedirect(self):
+        self.assertNotContains(self.resp, 'Instructors', msg_prefix="Instructors is displayed")
+    def test_InstructorCreateCourseRedirect(self):
+        self.assertNotContains(self.resp, 'Create Course', msg_prefix="Create Course is displayed")
+    def test_InstructorCreateSectionRedirect(self):
+        self.assertNotContains(self.resp, 'Create Section', msg_prefix="Create Section is displayed")
+    def test_InstructorCreateAccountRedirect(self):
+        self.assertNotContains(self.resp, 'Create Account', msg_prefix="Create Account is displayed")
+
+
+
