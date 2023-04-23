cdbd5e5427710f058bb18939bd8fb982f3925bbe
Andrew Le <112992941+ale202008@users.noreply.github.com>
2023-04-22 23:43:34 -0500

    Made some tests for Sections to give a bit of guideline. Gonna split testDirectory tests into multiple classes and methods later.

--- TASchedulerWebApp/AcceptanceTests/testCourses.py
+++ TASchedulerWebApp/AcceptanceTests/testCourses.py
@@ -12,10 +12,9 @@ class AddCourseTestCases(TestCase):
     def test_CourseCreation(self):
         # Will check if user has the courses added to themselves
         # Also inherently checks if everything is same, fields, elements, all that stuff
-        self.Instructor = User.objects.create(username = "Instrurctor" , password = "Instructor")
         resp = self.UserClient.post('/AddCoursePage/', {'CourseName': self.Course1_name, 'CourseNumber': self.Course1_id})
-        listCourses = list(Course.objects.all())
-        self.assertEqual(resp.context['Courses'], listCourses, msg = "Course doesn't exist within the database despite being added.")
+        list_Courses = list(Course.objects.all())
+        self.assertEqual(resp.context['Courses'], list_Courses, msg = "Course doesn't exist within the database despite being added.")
 
     def test_NoNameCourse(self):
         resp = self.UserClient.post('/AddCoursePage/', {'CourseName': "", 'CourseNumber': 12})
@@ -30,5 +29,7 @@ class AddCourseTestCases(TestCase):
         self.assertEqual(resp.context['message'], "course not created.", msg = "Course name was not provided, but still processed.")
 
     # Need test to make sure that when user enters course page, all courses for user shows up.
+
+
     # Need test to make sure that when SuperUser adds a course, course shows up for course page.
     # Need tests to reflect Instructor field, ManyToManyField for courses for users.
