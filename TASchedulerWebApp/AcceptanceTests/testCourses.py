0d9dc28874e97d6dfa7d1dae951a6bc8b6a66fe6
Andrew Le <112992941+ale202008@users.noreply.github.com>
2023-04-22 22:10:38 -0500

    Finished testCourses.py

--- TASchedulerWebApp/AcceptanceTests/testCourses.py
+++ TASchedulerWebApp/AcceptanceTests/testCourses.py
@@ -13,7 +13,23 @@ class CourseTestCases(TestCase):
     def test_CourseCreation(self):
         # Will check if user has the courses added to themselves
         # Also inherently checks if everything is same, fields, elements, all that stuff
+        self.Instructor = User.objects.create(username = "Instrurctor" , password = "Instructor")
         resp = self.UserClient.post('/AddCoursePage/', {'CourseName': self.Course1_name, 'CourseNumber': self.Course1_id})
         listCourses = list(Course.objects.all())
         self.assertEqual(resp.context['Courses'], listCourses, msg = "Course doesn't exist within the database despite being added.")
 
+    def test_NoNameCourse(self):
+        resp = self.UserClient.post('/AddCoursePage/', {'CourseName': "", 'CourseNumber': 12})
+        self.assertEqual(resp.context['message'], "course not created.", msg = "Course name was not provided, but still processed.")
+
+    def test_NoIDCourse(self):
+        resp = self.UserClient.post('/AddCoursePage/', {'CourseName': "COMPSCI", 'CourseNumber': ""})
+        self.assertEqual(resp.context['message'], "course not created.", msg = "Course name was not provided, but still processed.")
+
+    def test_NoInstructorCourse(self):
+        resp = self.UserClient.post('/AddCoursePage/', {'CourseName': "", 'CourseNumber': ""})
+        self.assertEqual(resp.context['message'], "course not created.", msg = "Course name was not provided, but still processed.")
+
+    # Need test to make sure that when user enters course page, all courses for user shows up.
+    # Need test to make sure that when SuperUser adds a course, course shows up for course page.
+    # Need tests to reflect Instructor field, ManyToManyField for courses for users.
