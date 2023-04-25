48d38b96cb1fc606d250783506b17ed364863421
Miguel Garcia <113084671+heavdem@users.noreply.github.com>
2023-04-25 00:31:03 -0500

    edit to urls

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from TASchedulerWebApp.views import *
-from . import views
+from .views import InstructorCreationView
+
 
 
 urlpatterns = [
