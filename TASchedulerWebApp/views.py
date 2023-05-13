0f997f89838a6f3495619f23232d940dea96b06f
Andrew Le <112992941+ale202008@users.noreply.github.com>
2023-05-13 16:43:43 -0500

    Touched up my code, split stuff into different functions.

--- TASchedulerWebApp/views.py
+++ TASchedulerWebApp/views.py
@@ -302,12 +302,10 @@ class DeleteSectionPage(View):
 class AssignSection(View):
     def get(self, request):
         # Similar to what Benji did for creating sections. Will need to select a course to get sections.
-        course_list = list(Course.objects.all())
-        return render(request, "AssignSection.html", {"course_list": course_list})
+        return render(request, "AssignSection.html", {"course_list": self.get_courses()})
 
     def post(self, request):
         todo = request.POST.get('chosen')
-        course_list = list(Course.objects.all())
         # Will go through different if conditionals to decide which operation to do.
         # Copied from SectionPage code in presenting the information to the user.
         if todo == "Back":
@@ -317,57 +315,103 @@ class AssignSection(View):
             # Got rid of the try/catch block as it seems there might be too many exceptions to cover in one
             # to simplify until necessary, code will run without try block.
             course = Course.objects.get(id = course_num)
-            course_sections = list(Section.objects.filter(Course = course))
-            if course_sections.__len__() == 0:
-                return render(request, "AssignSection.html", {'message': 'No sections exist for this course.', 'Sections': course_sections, 'course_list': course_list})
-            else:
-                # If course sections list is not 0, meaning that we do have a section that exists we shall send back
-                # the list of Instructor and Teacher Assistant users to be listed and chosen to assign.
-                teacher_assistant_list = list(User.objects.filter(is_superuser = False, is_staff = False))
-                instructors_list = list(User.objects.filter(is_superuser = False, is_staff = True))
-                if teacher_assistant_list.__len__() == 0:
-                    return render(request, "AssignSection.html", {'message': 'There exists no Teacher Assistants'})
-                elif instructors_list.__len__() == 0:
-                    return render(request, "AssignSection.html", {'message': 'There exists no Instructors'})
-                else:
-                    return render(request, 'AssignSection.html', {'course_sections': course_sections, 'teacher_assistant_list': teacher_assistant_list, 'instructor_list': instructors_list})
+            course_sections = self.get_course_sections(course)
+            teacher_assistant_list = self.get_teacher_assistants()
+            instructors_list = self.get_course_instructors(course)
+            return self.validifiy_conditions(request, course_sections, teacher_assistant_list, instructors_list)
         if todo == 'Assign':
             # Getting the model objects so that I can change section fields for Instructors and Teacher Assistants
             # if necessary
             section = Section.objects.get(id = request.POST.get('select_section'))
-            if request.POST.get('select_instructor') == "":
-                instructor = None
-            else:
-                instructor = User.objects.get(first_name=request.POST.get('select_instructor'))
+            instructor = self.set_instructor(request.POST.get('select_instructor'))
+            teacher_assistant = self.set_teacher_assistant(request.POST.get('select_teacher_assistant'))
+        return self.assign_section(request, instructor, section, teacher_assistant)
 
-            if request.POST.get('select_teacher_assistant') == "":
-                teacher_assistant = None
-            else:
-                teacher_assistant = User.objects.get(first_name=request.POST.get('select_teacher_assistant'))
 
-            # Checks to see if that section's Instructor field contains the instructor selected, will probably
-            # change it so that only this course's instructor shows up
-            if instructor != None and section.Course.Instructor != instructor:
-                return render(request, "AssignSection.html", {'message2': 'Instructor does not teach this sections course', 'course_list': course_list})
-            # Checks to see if the selected TA existed for a section already
-            elif teacher_assistant != None and Section.objects.filter(TeacherAssistant = teacher_assistant).exists():
-                return render(request, "AssignSection.html",{'message2': 'Teacher Assistant is already assigned to a section','course_list': course_list})
+    def validifiy_conditions(self, request, course_sections, teacher_assistant_list, instructors_list):
+        if self.is_Empty(course_sections):
+            message = "No sections for this course"
+            return render(request, "AssignSection.html", {'message': message, 'Sections': course_sections,
+                                                          'course_list': self.get_courses()})
+        else:
+            # If course sections list is not 0, meaning that we do have a section that exists we shall send back
+            # the list of Instructor and Teacher Assistant users to be listed and chosen to assign.
+            if self.is_Empty(teacher_assistant_list):
+                message = "There exists no Teacher Assistants"
+                return render(request, "AssignSection.html", {'message': message})
+            elif self.is_Empty(instructors_list):
+                message = 'There exists no Instructors'
+                return render(request, "AssignSection.html", {'message': message})
             else:
-                section.Instructor = instructor
-                section.TeacherAssistant = teacher_assistant
-                section.save()
-                return render(request, "AssignSection.html",{'message2': 'Assign successful for section: ' + section.id, 'course_list': course_list, 'section_saved': section})
+                return render(request, 'AssignSection.html', {'course_sections': course_sections,
+                                                              'teacher_assistant_list': teacher_assistant_list,
+                                                              'instructor_list': instructors_list,
+                                                              'course_list': self.get_courses()})
+    def set_instructor(self, select_instructor):
+        if select_instructor == "":
+            return None
+        return User.objects.get(email=select_instructor)
 
-        return render(request, "AssignSection.html")
+    def set_teacher_assistant(self, select_teacher_assistant):
+        if select_teacher_assistant == "":
+            return None
+        return User.objects.get(email=select_teacher_assistant)
+    def get_courses(self):
+        # Gets and return a list of all courses.
+        course_list = list(Course.objects.all())
+        return course_list
+
+    def get_course_sections(self, course):
+        course_section_list = list(Section.objects.filter(Course = course))
+        return course_section_list
+
+    def get_course_instructors(self, course):
+        instructors_list = list(User.objects.filter(email = course.Instructor.email))
+        return instructors_list
+
+    def get_teacher_assistants(self):
+        teacher_assistant_list = list(User.objects.filter(is_superuser=False, is_staff=False))
+        return teacher_assistant_list
+
+    def is_Empty(self, list):
+        if not bool(list):
+            return True
+        return False
+
+
+    def assign_section(self, request, instructor, section, teacher_assistant):
+        # Checks to see if that section's Instructor field contains the instructor selected, will probably
+        # change it so that only this course's instructor shows up
+        message = ''
+
+        # context = {'message2': message, 'course_sections': self.get_course_sections(course),
+        #            'teacher_assistant_list': self.get_teacher_assistants(),
+        #            'instructor_list': self.get_course_instructors(course),
+        #            'course_list': self.get_courses()}
+
+        context = {'message2': message, 'course_list': self.get_courses()}
+        if instructor != None and section.Course.Instructor != instructor:
+            message = 'Instructor does not teach this sections course'
+            context['message2'] = message
+            return render(request, "AssignSection.html", context)
+        # Checks to see if the selected TA existed for a section already
+        elif teacher_assistant != None and Section.objects.filter(TeacherAssistant=teacher_assistant).exists():
+            message = 'Teacher Assistant is already assigned to a section'
+            context['message2'] = message
+            return render(request, "AssignSection.html", context)
+        else:
+            section.Instructor = instructor
+            section.TeacherAssistant = teacher_assistant
+            section.save()
+            message = 'Assign successful for section: ' + section.id
+            context['message2'] = message
+            return render(request, "AssignSection.html", context)
+
 
 class Notifications(View):
     def get(self, request):
         self.User = request.user
-        if self.permitted_user(User):
-            return render(request, 'notifications.html',
-                          {'notifications': self.getNotifications(self.User), 'permission': self.permitted_user(self.User),
-                           'instructors': self.getInstructors(), 'teacherassistants': self.getTeacherAssistants()})
-        return render(request, 'notifications.html', {'notifications': self.getNotifications(self.User), 'permission': self.permitted_user(self.User)})
+        return self.display_render(self.User, request, "")
     def post(self, request):
         self.User = request.user
         todo = request.POST.get('chosen')
@@ -375,22 +419,28 @@ class Notifications(View):
             return redirect('directory')
         else:
             Users = request.POST.getlist('select_user')
+            if bool(Users) == False:
+                context = 'Please select recipients.'
+                return self.display_render(self.User, request, context)
             notification = request.POST.get('new_notification')
             self.makeNotification(Users, notification)
+            context = 'Notification sent!'
+        return self.display_render(self.User, request, context)
 
-        if self.permitted_user(self.User):
+    def display_render(self, User, request, context):
+        if User.is_superuser == True:
             return render(request, 'notifications.html',
-                          {'notifications': self.getNotifications(self.User), 'permission': self.permitted_user(self.User),
-                           'instructors': self.getInstructors(), 'teacherassistants': self.getTeacherAssistants()})
-        return render(request, 'notifications.html',
-                      {'notifications': self.getNotifications(self.User), 'permission': self.permitted_user(self.User)})
-
-    def permitted_user(self, User):
-        if User.is_superuser or User.is_staff:
-            return True
+                          {'notifications': self.getNotifications(self.User), 'permission': True,
+                           'instructors': self.getInstructors(), 'teacherassistants': self.getTeacherAssistants(),
+                           'super_user': True, 'message': context})
+        elif User.is_staff == True:
+            return render(request, 'notifications.html',
+                          {'notifications': self.getNotifications(self.User), 'permission': True,
+                           'teacherassistants': self.getTeacherAssistants(), 'super_user': False,
+                           'message': context})
         else:
-            return False
-
+            return render(request, 'notifications.html', {'notifications': self.getNotifications(self.User),
+                                                          'permission': False})
     def getInstructors(self):
         instructor_list = list(User.objects.filter(is_staff=True))
         return instructor_list
